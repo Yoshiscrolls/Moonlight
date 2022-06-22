cb33c1adc0a8e473ae2d4455a9a041ee94b6c963
Yoshiscrolls <104365779+Yoshiscrolls@users.noreply.github.com>
2022-06-22 22:25:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let moon = document.getElementById(`moon`);
+let stars = document.getElementById(`stars`);
+let mountainBehind = document.getElementById(`mountains_behind`);
+let buttonExplore = document.getElementById(`btn`);
+let text = document.getElementById(`text`);
+let header = document.querySelector(`header`);
+
+window.addEventListener(`scroll`, function () {
+let value = window.scrollY;
+
+stars.style.left = value * 0.25 + `px`;
+moon.style.top = value + `px`;
+mountainBehind.style.top = value * 0.5 + `px`;
+buttonExplore.style.marginTop = value * 1.5 + `px`;
+text.style.marginTop = value * 1 + `px`;
+text.style.marginRight = value * 3 + `px`;
+header.style.top = value * 0.5 + `px`;
+});
